d2b5e41bf67bb91e0cfd7f16844c9f48a2136607
TeaInMud <mytotallycoolgmail@gmail.com>
2026-06-29 20:41:44 -0500

    Update better-git package URL in tpm.reg.py

--- tpm/registry/tpm.reg.py
+++ tpm/registry/tpm.reg.py
@@ -1,5 +1,5 @@
 PACKAGES = {
     "juice": "https://raw.githubusercontent.com/TeaInMud/Tea-Package-Manager/refs/heads/master/tpm/packages/juice.sh",
     "skibidi": "https://raw.githubusercontent.com/TeaInMud/Tea-Package-Manager/refs/heads/master/tpm/packages/skibidi.sh",
-    "better-git": "https://raw.githubusercontent.com/TeaInMud/TPM-Custom-Packages/refs/heads/main/better-git.py"
+    "better-git": "https://raw.githubusercontent.com/TeaInMud/Tea-Package-Manager/refs/heads/master/tpm/packages/better-git.py"
 }
