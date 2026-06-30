70bba23d0d3b011a47141fb22da2513d65866407
TeaInMud <mytotallycoolgmail@gmail.com>
2026-06-29 21:10:40 -0500

    Add 'notes' package to PACKAGES dictionary

--- tpm/registry/tpm.reg.py
+++ tpm/registry/tpm.reg.py
@@ -1,5 +1,6 @@
 PACKAGES = {
     "juice": "https://raw.githubusercontent.com/TeaInMud/Tea-Package-Manager/refs/heads/master/tpm/packages/juice.sh",
     "skibidi": "https://raw.githubusercontent.com/TeaInMud/Tea-Package-Manager/refs/heads/master/tpm/packages/skibidi.sh",
-    "better-git": "https://raw.githubusercontent.com/TeaInMud/Tea-Package-Manager/refs/heads/master/tpm/packages/better-git.py"
+    "better-git": "https://raw.githubusercontent.com/TeaInMud/Tea-Package-Manager/refs/heads/master/tpm/packages/better-git.py",
+    "notes": "https://raw.githubusercontent.com/TeaInMud/Tea-Package-Manager/refs/heads/master/tpm/packages/notes.py"
 }
