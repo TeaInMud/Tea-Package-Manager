defd845cf4bf517d84d4b9186916275abcdd2881
TeaInMud <mytotallycoolgmail@gmail.com>
2026-06-29 20:25:17 -0500

    Add package URLs to tpm.reg.py

---
+++ tpm/registry/tpm.reg.py
@@ -0,0 +1,5 @@
+PACKAGES = {
+    "juice": "https://raw.githubusercontent.com/TeaInMud/juice/refs/heads/main/juice.sh",
+    "skibidi": "https://raw.githubusercontent.com/TeaInMud/TPM-Custom-Packages/refs/heads/main/skibidi.sh",
+    "better-git": "https://raw.githubusercontent.com/TeaInMud/TPM-Custom-Packages/refs/heads/main/better-git.py"
+}
